89d2196fb950cab8f24e09cfe8557625c3de3dba
Daniil <134328263+DementevDaniil@users.noreply.github.com>
2024-06-13 16:45:03 +0300

    Update Map.tsx

--- ReactWeatherMap/Map.tsx
+++ ReactWeatherMap/Map.tsx
@@ -3,8 +3,36 @@ import Map from './node_modules/react-map-gl/dist/es5/exports-maplibre';
 import { FetchWeatherData } from './Weather';
 import { WriteWeatherData } from './WeatherWidget';
 
+type WeatherData = {
+    hourly: {
+        time: Date[];
+        temperature2m: Float32Array;
+        relativeHumidity2m: Float32Array;
+        precipitation: Float32Array;
+        surfacePressure: Float32Array;
+        cloudCover: Float32Array;
+        windSpeed10m: Float32Array;
+        windDirection10m: Float32Array;
+    };
+};
+
 export function CreateMap() {
-    // const [isWeatherDataFetched, setIsWeatherDataFetched] = useState(false)
+    const wd: WeatherData = {
+        hourly: {
+            time: [],
+            temperature2m: new Float32Array([]),
+            relativeHumidity2m: new Float32Array([]),
+            precipitation: new Float32Array([]),
+            surfacePressure: new Float32Array([]),
+            cloudCover: new Float32Array([]),
+            windSpeed10m: new Float32Array([]),
+            windDirection10m: new Float32Array([])
+        }
+    };
+    const [weatherData, setWeatherData] = useState(wd);
+    const [isWeatherDataFetched, setWIseatherDataFetched] = useState(false);
+    const [oldLongitude, setOldLongitude] = useState(-480);
+    const [oldLatitude, setOldLatitude] = useState(-350);
     return (
         <Map
             id="map"
@@ -24,12 +52,13 @@ export function CreateMap() {
             dragRotate={false}
             touchZoomRotate={false}
             onClick={(e) => {
-                let weatherData;
+                let lat = e.lngLat.lat;
                 let lng = e.lngLat.lng;
                 if (Math.abs(lng) > 180) {
                     lng -= 360 * Math.sign(lng);
                 }
-                FetchWeatherData(lng, e.lngLat.lat).then((weatherData) => {
+                FetchWeatherData(lng, lat).then((result) => {
+                    setWeatherData(result);
                     WriteWeatherData(weatherData);
                 });
             }}
